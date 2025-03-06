6006f3b2d461eedbac8ebc926aafc3703e286b5e
Christoph Muellner <33160506+chrismuellner@users.noreply.github.com>
2025-03-06 10:28:03 +0100

    Exclude sample pod from operator injection (#28)

--- hack/make/deploy.mk
+++ hack/make/deploy.mk
@@ -1,22 +1,22 @@
 deploy:
 	helm upgrade bootstrapper-testing-sample hack/testing/helm-sample \
-			--install \
-			--namespace bootstrapper-test \
-			--create-namespace \
-			--set image.repository=$(IMAGE) \
-			--set image.tag=$(TAG) \
-			--atomic
+		--install \
+		--namespace bootstrapper-test \
+		--create-namespace \
+		--set image.repository=$(IMAGE) \
+		--set image.tag=$(TAG) \
+		--atomic
 
 deploy/custom:
 	helm upgrade bootstrapper-testing-sample hack/testing/helm-sample \
-			--install \
-			--namespace bootstrapper-test \
-			--create-namespace \
-			--set image.repository=$(IMAGE) \
-			--set image.tag=$(TAG) \
-			--atomic \
-			-f hack/testing/helm-sample/_values.yaml
+		--install \
+		--namespace bootstrapper-test \
+		--create-namespace \
+		--set image.repository=$(IMAGE) \
+		--set image.tag=$(TAG) \
+		--atomic \
+		-f hack/testing/helm-sample/_values.yaml
 
 undeploy:
 	helm uninstall bootstrapper-testing-sample \
-			--namespace bootstrapper-test
+		--namespace bootstrapper-test
