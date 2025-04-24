bbb7dede8737638568d423d87226d6de5e588e65
Marcell Sevcsik <31651557+0sewa0@users.noreply.github.com>
2025-04-24 09:20:26 +0200

    Fix the version printing (#55)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,6 +1,8 @@
 package version
 
 import (
+	"runtime/debug"
+
 	"github.com/go-logr/logr"
 )
 
@@ -10,15 +12,45 @@ var (
 	AppName = "dynatrace-bootsrapper"
 
 	// Version contains the version of the Bootstrapper. Assigned externally.
-	Version = "snapshot"
+	Version = ""
 
 	// Commit indicates the Git commit hash the binary was build from. Assigned externally.
 	Commit = ""
 
 	// BuildDate is the date when the binary was build. Assigned externally.
 	BuildDate = ""
+
+	ModuleSum = ""
 )
 
+func init() {
+	i, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+
+	if Version == "" {
+		Version = i.Main.Version
+	}
+
+	ModuleSum = i.Main.Sum
+
+}
+
 func Print(log logr.Logger) {
-	log.Info("version info", "name", AppName, "version", Version, "commit", Commit, "build_date", BuildDate)
+	keyValues := []any{"name", AppName, "version", Version}
+
+	if ModuleSum != "" {
+		keyValues = append(keyValues, "module-sum", ModuleSum)
+	}
+
+	if Commit != "" {
+		keyValues = append(keyValues, "commit", Commit)
+	}
+
+	if BuildDate != "" {
+		keyValues = append(keyValues, "build_date", BuildDate)
+	}
+
+	log.Info("version info", keyValues...)
 }
